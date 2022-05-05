fb82da6d820c600d15688531da0307873668648e
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-05-05 22:06:17 +0700

    Simplify listener dropdown people notifier

--- lib/src/view_model/people/people_dropdown_notifier.dart
+++ lib/src/view_model/people/people_dropdown_notifier.dart
@@ -4,20 +4,11 @@ import '../../../injection.dart';
 import 'people_action_notifier.dart';
 
 final getDropdownPeoples = FutureProvider.autoDispose((ref) async {
-  /// Jika ada [penambahan / update / delete] dari people
-  /// refresh provider [getLatestTenPeople]
-  ref.listen<PeopleActionState>(
-    peopleActionNotifier,
-    (_, state) {
-      state.deleteAsync.whenOrNull(data: (_) {
-        ref.invalidateSelf();
-      });
-
-      state.insertOrUpdateAsync.whenOrNull(data: (_) {
-        ref.invalidateSelf();
-      });
-    },
-  );
+  /// Jika ada [penambahan / update / delete] dari people, refresh provider ini
+  ref.listen<PeopleActionState>(peopleActionNotifier, (_, state) {
+    state.deleteAsync.whenOrNull(data: (_) => ref.invalidateSelf());
+    state.insertOrUpdateAsync.whenOrNull(data: (_) => ref.invalidateSelf());
+  });
 
   final result = await ref.watch(peopleRepository).get();
   return result.getOrElse(() => []);
