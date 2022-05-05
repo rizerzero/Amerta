66792eba7de37886f4038747cbafe0829ef2b809
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-05-05 22:07:16 +0700

    Use whenData instead whenDataOrNull

--- lib/src/view_model/people/people_dropdown_notifier.dart
+++ lib/src/view_model/people/people_dropdown_notifier.dart
@@ -6,8 +6,8 @@ import 'people_action_notifier.dart';
 final getDropdownPeoples = FutureProvider.autoDispose((ref) async {
   /// Jika ada [penambahan / update / delete] dari people, refresh provider ini
   ref.listen<PeopleActionState>(peopleActionNotifier, (_, state) {
-    state.deleteAsync.whenOrNull(data: (_) => ref.invalidateSelf());
-    state.insertOrUpdateAsync.whenOrNull(data: (_) => ref.invalidateSelf());
+    state.deleteAsync.whenData((_) => ref.invalidateSelf());
+    state.insertOrUpdateAsync.whenData((_) => ref.invalidateSelf());
   });
 
   final result = await ref.watch(peopleRepository).get();
